2f763e3510d4f718e3eab68cb0baf3fa2e654f90
Amar Jaiswal <aj.jaiswal007@gmail.com>
2022-03-29 20:01:49 +0530

    Added passphrase support

--- otsapp/main.py
+++ otsapp/main.py
@@ -1,40 +1,8 @@
-from typing import Optional
+from dotenv import load_dotenv
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Form, Request
-from fastapi.templating import Jinja2Templates
+from otsapp.routers import secrets
 
-from otsapp.services.secret_keeper import SecretKeeper
-from .dtos import SecretData
-
+load_dotenv()
 app = FastAPI(docs_url=None, redoc_url=None)
-
-templates = Jinja2Templates(directory="otsapp/templates")
-
-secret_keeper = SecretKeeper()
-
-
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "id": 123})
-
-
-@app.post("/secret")
-def create_secret(request: Request, secret: str = Form(...)):
-    key = secret_keeper.add_secret(data=SecretData(secret=secret))
-    return templates.TemplateResponse(
-        "secret.html",
-        {"request": request, "base_url": "http://127.0.0.1:8000/", "key": key},
-    )
-
-
-@app.get("/{key}")
-def read_secret(request: Request, key: str):
-    try:
-        data = secret_keeper.get_secret(key=key)
-    except KeyError:
-        return templates.TemplateResponse("invalid.html", {"request": request})
-
-    secret_keeper.remove_secret(key=key)
-    return templates.TemplateResponse(
-        "read_secret.html", {"request": request, "secret": data.secret}
-    )
+app.include_router(router=secrets.router, prefix="")
---
+++ otsapp/routers/secrets.py
@@ -0,0 +1,69 @@
+from typing import Optional
+from fastapi import Form, Request
+from fastapi import APIRouter
+
+from otsapp.dtos import SecretData
+
+from ..services.secret_keeper import SecretKeeper
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="otsapp/templates")
+secret_keeper = SecretKeeper()
+
+
+@router.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "id": 123})
+
+
+@router.post("/secret")
+def create_secret(
+    request: Request,
+    secret: str = Form(...),
+    passphrase: Optional[str] = Form(default=None),
+):
+    key = secret_keeper.add_secret(
+        data=SecretData(secret=secret, passphrase=passphrase)
+    )
+    return templates.TemplateResponse(
+        "secret_link.html",
+        {"request": request, "base_url": "http://127.0.0.1:8000/", "key": key},
+    )
+
+
+@router.get("/{key}")
+def get_secret(request: Request, key: str):
+    if not secret_keeper.is_key_valid(key=key):
+        return templates.TemplateResponse(
+            "invalid.html", {"request": request, "message": "Not found!"}
+        )
+
+    return templates.TemplateResponse("show_secret.html", {"request": request})
+
+
+@router.post("/{key}")
+def view_secret(
+    request: Request, key: str, passphrase: Optional[str] = Form(default=None)
+):
+    try:
+        data = secret_keeper.get_secret(key=key)
+        assert data.passphrase == passphrase
+    except KeyError:
+        return templates.TemplateResponse(
+            "invalid.html", {"request": request, "message": "Not found!"}
+        )
+
+    except AssertionError:
+        return templates.TemplateResponse(
+            "invalid.html",
+            {
+                "request": request,
+                "message": "Unauthorized. Pls enter correct passphrase",
+            },
+        )
+
+    secret_keeper.remove_secret(key=key)
+    return templates.TemplateResponse(
+        "read_secret.html", {"request": request, "secret": data.secret}
+    )
